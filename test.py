7ce75243457e31464a0b7d21d60334ec4525e818
cwangaw <cwangaw@utexas.edu>
2023-10-06 10:01:45 -0500

    clean up the codes

--- test.py
+++ test.py
@@ -2,14 +2,9 @@ from geogen import *
 from manuSolver import *
 from ngsolve import *
 
-from geogen import *
-from solver import *
-from ngsolve import *
-import matplotlib.pyplot as plt
-
-fractal_level = 0
+fractal_level = 4
 d = 1
 (mesh,l,L_p) = MakeGeometry(fractal_level)
 (gfu, flux_top, e) = SolveManuPoisson(mesh, d, L_p)
 
-print("error is", e)
+print("Error:", e)
